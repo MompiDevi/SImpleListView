b4bac0bd86193d89f4224451db27a360d4984574
Mompi <mompi.devi@2mee.com>
2020-03-29 18:35:57 +0530

    Added carousel

--- app/src/main/java/com/example/recyclerviewwcreation/MainActivity.java
+++ app/src/main/java/com/example/recyclerviewwcreation/MainActivity.java
@@ -5,8 +5,11 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import com.example.recyclerviewwcreation.Adapter.RecyclerViewAdapter;
+import com.synnapps.carouselview.CarouselView;
+import com.synnapps.carouselview.ImageListener;
 
 public class MainActivity extends AppCompatActivity {
     RecyclerView list;
@@ -14,6 +17,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CarouselView carousel = (CarouselView) findViewById(R.id.carousel);
+        carousel.setPageCount(3);
+        carousel.setImageListener(new ImageListener() {
+            @Override
+            public void setImageForPosition(int position, ImageView imageView) {
+                switch (position){
+                    case 0:
+                        imageView.setImageResource(R.drawable.image1);
+                        break;
+                    case 1:
+                        imageView.setImageResource(R.drawable.image2);
+                        break;
+                    default:
+                        imageView.setImageResource(R.drawable.image3);
+                }
+            }
+        });
+
         list = (RecyclerView)findViewById(R.id.list);
         String[] data = {"Test 1","Test 2", "Test 3", "Test 4"};
         RecyclerViewAdapter adapter = new RecyclerViewAdapter(MainActivity.this, data);
